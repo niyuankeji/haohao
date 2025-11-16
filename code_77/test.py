257e132dcb4578d3fb07052a88c2c073c3fbf259
77 <y876608475@gmail.com>
2025-11-16 17:33:14 +0800

    测试

--- code_77/test.py
+++ code_77/test.py
@@ -25,8 +25,8 @@ cookies = {
     "m_session": "eyJpdiI6InozUlIvZDFSOUNydHozZ1M1aldkM0E9PSIsInZhbHVlIjoiNUd5NFl5TDF5UlNJR2poRDJ2Tmp4WmZjeEQ0Q2NnczhRbzgvdXpGRlZXbGNFQXNuVWdaZFBuY29yMW5sbXJSbTZRUnc3RExxcHROWGc3ZWRaT3liTGVkaW81L0xpK3E3MkdzcG5raHRyMWoxWDBucitjWE5FaDB6VVpwc2RjSXgiLCJtYWMiOiI0MTkyMWFhMDI5N2QwMWI4NDhhOWVkMTY5MGZmYWFiNzBhNjVkMDBjYTBmNGI0Y2Q2MmZkOWIzOTRiOThhZjVmIiwidGFnIjoiIn0%3D",
     # "ShowFilter": "0",
 }
-url = "https://filmot.com/search/aaa/1/2"
-params = {"gridView": "1", "": "", "lang": "en"}
+url = "https://filmot.com/search/zqq/1/2"
+params = {"gridView": "1", "lang": "en"}
 proxies = {
     "http": f"http://td-customer-SOluI6kkrdk2-sessid-{generate_secure_random_string()}-sesstime-15:rEpTA530j0i6@43.153.55.54:9999",
     "https": f"http://td-customer-SOluI6kkrdk2-sessid-{generate_secure_random_string()}-sesstime-15:rEpTA530j0i6@43.153.55.54:9999",
