a27aa7f74c54d8d017c649c8a49fd3e6a3aba8ba
77 <y876608475@gmail.com>
2025-11-16 17:18:30 +0800

    测试

--- code_77/filmont_ez.py
+++ code_77/filmont_ez.py
@@ -279,14 +279,16 @@ async def create_conn_from_ez():
 
 async def create_conn_from_hcaptcha():
     cookies = {
-        "m_session": "eyJpdiI6InozUlIvZDFSOUNydHozZ1M1aldkM0E9PSIsInZhbHVlIjoiNUd5NFl5TDF5UlNJR2poRDJ2Tmp4WmZjeEQ0Q2NnczhRbzgvdXpGRlZXbGNFQXNuVWdaZFBuY29yMW5sbXJSbTZRUnc3RExxcHROWGc3ZWRaT3liTGVkaW81L0xpK3E3MkdzcG5raHRyMWoxWDBucitjWE5FaDB6VVpwc2RjSXgiLCJtYWMiOiI0MTkyMWFhMDI5N2QwMWI4NDhhOWVkMTY5MGZmYWFiNzBhNjVkMDBjYTBmNGI0Y2Q2MmZkOWIzOTRiOThhZjVmIiwidGFnIjoiIn0%3D"
+        # "XSRF-TOKEN": "eyJpdiI6IlhNNzF6VjBlaWxKdUJRbGd4ZmtwMUE9PSIsInZhbHVlIjoiN2tldzN2S0F0djhaV0ZMWW1vd0xjWGFTRnlic1lCYzY2UitkbUwwek96TFdzd0VZV3ZvbW00aXBiRldCVW5ManlDYTNzQTM3Q1BWRkUrdS95YVk3V3BRMVRQanoyR0l2TEtYaG1rWWY2MjdkUWJxUlpPbW5DNmpUVHRsclZtQ3YiLCJtYWMiOiI0MzdhYjg3NDIyNDEyNmFlMmE3NTQ3MmFkMWY2NzU4MDNjMzY0MzM4YzExMTAxMmI2NGZlZjk0N2M1OTdhNmQ1IiwidGFnIjoiIn0%3D",
+        "m_session": "eyJpdiI6InozUlIvZDFSOUNydHozZ1M1aldkM0E9PSIsInZhbHVlIjoiNUd5NFl5TDF5UlNJR2poRDJ2Tmp4WmZjeEQ0Q2NnczhRbzgvdXpGRlZXbGNFQXNuVWdaZFBuY29yMW5sbXJSbTZRUnc3RExxcHROWGc3ZWRaT3liTGVkaW81L0xpK3E3MkdzcG5raHRyMWoxWDBucitjWE5FaDB6VVpwc2RjSXgiLCJtYWMiOiI0MTkyMWFhMDI5N2QwMWI4NDhhOWVkMTY5MGZmYWFiNzBhNjVkMDBjYTBmNGI0Y2Q2MmZkOWIzOTRiOThhZjVmIiwidGFnIjoiIn0%3D",
+        # "ShowFilter": "0",
     }
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
     conn_id = str(uuid.uuid4())[:8]
     return CloudflareConn(
         conn_id=conn_id,
         success_count=0,
-        proxy="http://ytbpr78721:btVBqzWYsuT8@ytbzero.123proxy.cn:7317",
+        proxy="http://td-customer-SOluI6kkrdk2:rEpTA530j0i6@43.153.55.54:9999",
         cookies=cookies,
         user_agent=user_agent,
         use_datetime=datetime.datetime.now(),
